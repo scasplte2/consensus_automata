54ebb30e8823c4d5cd342fe9dfd8163715890146
Aaron Schutza <aaronschutza@gmail.com>
2021-10-26 15:56:40 -0500

    revised sum composition vectors

--- specs/crypto/signing/KES-Ed25519-Blake2b256-SC/sum_vectors.py
+++ specs/crypto/signing/KES-Ed25519-Blake2b256-SC/sum_vectors.py
@@ -1,6 +1,22 @@
 from sum_comp import *
 
 
+readme_header = "## Description\n"\
+                +"Test vectors for Topl implementation of a single tree key evolving signature scheme in the construction of [Malkin-Micciancio-Miner](https://cseweb.ucsd.edu/~daniele/papers/MMM.pdf). A full specification including protocol box descriptions is available in this repo.\n"\
+                +"All values below are Hex encoded byte representations unless otherwise specified.\n"\
+                +"\nTable of Contents\n"\
+                +"- [Test vector - 1](#test-vector---1)\n"\
+                +"- [Test vector - 2](#test-vector---2)\n"\
+                +"- [Test vector - 3](#test-vector---3)\n"\
+                +"- [Test vector - 4](#test-vector---4)\n"\
+                +"- [Test vector - 5](#test-vector---5)\n"\
+                +"- [Test vector - 6](#test-vector---6)\n"\
+                +"- [Test vector - 7](#test-vector---7)\n"\
+                +"- [Test vector - 8](#test-vector---8)\n"\
+                +"- [Test vector - 9](#test-vector---9)\n"\
+                +"- [Test vector - 10](#test-vector---10)"
+
+
 messages = ["language shares a common tongue",
             "huddled masses with socks",
             "arriving on time and late",
@@ -45,32 +61,44 @@ seeds = ["lineage",
          "pretend"]
 
 
-def line():
-    print("--------------------------------------------------------------------------------------------------")
+print(readme_header)
 
 
-def sum_test_vector(message: str, seed: str, h: int, t_range: list[int], test_number: int):
-    line()
-    print("Sum Test Vector: "+str(test_number))
-    print("Seed String: \""+seed+"\"")
-    print("Seed Bytes: ["+seed.encode().hex()+"]")
-    print("h =", h)
-    print("Message String: \"" + message + "\"")
-    print("Message Bytes: ["+message.encode().hex()+"]")
-    sum_key = key_gen_sum(seed.encode(), h)
-    print("Sum Composition Verification Key:")
-    print("["+verification_key_sum(sum_key).hex()+"]")
-    print("Sum Composition Secret Key, t = 0:")
+def sum_test_vector(message: str, seed_str: str, h: int, t_range: list[int], test_number: int):
+    seed = hash(seed_str.encode())
+    print("## Test Vector - "+str(test_number))
+    print("### Description "
+          + "\n Generate and verify a specified sum composition signature at "
+          + "`t = "+str(t_range)
+          + "` using a provided seed, message, and height")
+    print("### Inputs")
+    print("- Seed Bytes:")
+    print("```\n"+seed.hex()+"\n```")
+    print("- h (int):")
+    print("\n```"+str(h)+"```\n")
+    print("- Message (string):")
+    print("```\n\"" + message + "\"\n```")
+    print("- Message (bytes) with `utf-8` encoding:")
+    print("```\n"+message.encode('utf-8').hex()+"\n```")
+    print("### Outputs")
+    sum_key = key_gen_sum(seed, h)
+    print("- Sum Composition Verification Key:")
+    print("```\n"+verification_key_sum(sum_key).hex()+"\n```")
+    print("- Sum Composition Secret Key, t = 0:")
+    print("```")
     print_vector(sum_key)
-    line()
+    print("```")
     for j in t_range:
         sum_key = key_update_sum(sum_key, j)
-        sigma = sign_sum(sum_key, message.encode())
-        print("sigma t = "+str(j)+":")
+        sigma = sign_sum(sum_key, message.encode('utf-8'))
+        print("- Sigma t = "+str(j)+":")
+        print("```")
         sigma.print()
-        line()
-    print("Sum Composition Secret Key, t = "+str(key_time_sum(sum_key))+":")
+        print("```")
+    print("- Sum Composition Secret Key, t = "+str(key_time_sum(sum_key))+":")
+    print("```")
     print_vector(sum_key)
+    print("```")
 
 
 sum_test_vector(messages[0], seeds[0], 1, [0, 1], 1)
